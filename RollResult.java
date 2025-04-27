316ed1f61a88e4e575cadf508c400e4e8eeb359f
ThanhHang060605 <phamthanhhangqp66@gmai.com>
2025-04-27 23:02:27 +0700

    Refactor RollResult: Improve And Fix Source Code_1

--- RollResult.java
+++ RollResult.java
@@ -69,7 +69,7 @@ public class RollResult { // Lỗi cú pháp: Thêm dấu '{' mở đầu classc
     }
 	/**
      * Refactor: Sửa lỗi cú pháp: Thêm dấu '}' ở cuối class.
-	 *Lý do: Trình biên dịch Java không tìm thấy dấu '}' để đóng class -> Và báo lỗi 
+	 *Lý do: Trình biên dịch Java không tìm thấy dấu '}' để đóng class -> Và thông báo lỗi 
 	 */
 
 }
